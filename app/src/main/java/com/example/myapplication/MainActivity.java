943d040dc134b1c9ccf646e977ee394bea33d5c1
YaxinJoy <YWAN008@e.ntu.edu.sg>
2021-09-28 16:23:40 +0800

    medicine reminder ui2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,11 +23,6 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseReference reference;
-    RecyclerView ourMR;
-    MRAdapter mrAdapter;
-    ArrayList<MedicineReminder> list;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -36,32 +31,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //working with data
-        ourMR = findViewById(R.id.ourMR);
-        ourMR.setLayoutManager(new LinearLayoutManager(this));
-        list = new ArrayList<MedicineReminder>();
-
-        //get data from firebase
-        reference = FirebaseDatabase.getInstance().getReference().child("Medicine Reminder");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //set code
-                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
-                    MedicineReminder p = dataSnapshot1.getValue(MedicineReminder.class);
-                    list.add(p);
-                }
-                mrAdapter = new MRAdapter(MainActivity.this, list);
-                ourMR.setAdapter(mrAdapter);
-                mrAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //set code to show an error
-                Toast.makeText(getApplicationContext(), "No data", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
     public void sendMessage(View view) {
         Intent intent = new Intent(this, CreateMr.class);
